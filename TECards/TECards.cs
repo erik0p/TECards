f6be571f775b95dffcc9f04f795576010c94859b
erik0p <topexplorer71@gmail.com>
2025-01-01 11:33:53 -0600

    ready for 1.0

--- TECards/TECards.cs
+++ TECards/TECards.cs
@@ -36,7 +36,7 @@ namespace TECards
 
         private const string ModId = "com.thirdeye.tecards";
         private const string ModName = "TECards";
-        private const string Version = "0.1.0";
+        private const string Version = "1.0.0";
         public const string ModInitials = "TE";
         public static CardCategory Candy;
 
